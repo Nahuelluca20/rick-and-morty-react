3c6e64fee39ed3818753fe1048d354e42d26c441
Nahuel Luca <nahuelluca@protonmail.com>
2020-08-19 19:37:14 -0300

    Updates

---
+++ precache-manifest.908a32838e9f35c6c9f20dc4776ab1e5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5661e686bbd9e31bab5402effb7d646",
+    "url": "/rick-and-morty-react/index.html"
+  },
+  {
+    "revision": "e1b76b67f2c08b0cf1d8",
+    "url": "/rick-and-morty-react/static/css/main.65328da8.chunk.css"
+  },
+  {
+    "revision": "17e735f6c28c4f03ae7c",
+    "url": "/rick-and-morty-react/static/js/2.6ea4bf9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick-and-morty-react/static/js/2.6ea4bf9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b76b67f2c08b0cf1d8",
+    "url": "/rick-and-morty-react/static/js/main.072ad805.chunk.js"
+  },
+  {
+    "revision": "bdcae43a429400694a77",
+    "url": "/rick-and-morty-react/static/js/runtime-main.474bc83c.js"
+  },
+  {
+    "revision": "4b713cfeb6d51be8acd2325c8b8b456a",
+    "url": "/rick-and-morty-react/static/media/arrow.4b713cfe.svg"
+  },
+  {
+    "revision": "ee1fc1ed65c7356835cb0299ff645704",
+    "url": "/rick-and-morty-react/static/media/coding.ee1fc1ed.svg"
+  },
+  {
+    "revision": "1f33e75d7b7df995b1609f8573ac8c9b",
+    "url": "/rick-and-morty-react/static/media/github.1f33e75d.svg"
+  },
+  {
+    "revision": "bb026c4d996a5d563f7fceb5166175e1",
+    "url": "/rick-and-morty-react/static/media/instagram.bb026c4d.svg"
+  },
+  {
+    "revision": "77c86913943d8217e607977f947bf352",
+    "url": "/rick-and-morty-react/static/media/left-arrow.77c86913.svg"
+  },
+  {
+    "revision": "59605127dffb786ac6bcb369a44f4e09",
+    "url": "/rick-and-morty-react/static/media/portal.59605127.png"
+  },
+  {
+    "revision": "efd2d130ddcf3bf1f4e04356915078b6",
+    "url": "/rick-and-morty-react/static/media/rym-logo.efd2d130.png"
+  },
+  {
+    "revision": "7e145c9eb60da95379f070be9920eca5",
+    "url": "/rick-and-morty-react/static/media/twitter.7e145c9e.svg"
+  }
+]);
